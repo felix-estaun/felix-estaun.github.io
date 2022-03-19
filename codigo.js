b40c29d169189f01adabad85d295d612b989148b
felix.estaun <felixest7@hotmail.com>
2022-03-19 15:24:14 +0100

    finish

--- codigo.js
+++ codigo.js
@@ -2,7 +2,7 @@ function checkPswd() {
     var gud = "2022sid0s";
     var pass = document.getElementById('contra').value;
     if (gud == pass) {
-        document.getElementById('resp').innerHTML = 'AAAAAAAAAAAAAAa'
+        document.getElementById('resp').innerHTML = "<img src=\"Turtle.png\" width=\"100\">¡Feliz cumpleaños, puerco!<img src=\"Turtle.png\" width=\"100\"><br>K7G6G-0Y84V-X46L(erre)"
     }
     else {
         document.getElementById('resp').innerHTML = pass;
